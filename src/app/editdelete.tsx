60390b7ea8cd96df77ff6626d95e2ba1371e90f5
Saadgithubit <saad45257@gmail.com>
2024-09-30 15:24:28 +0500

    add modal

--- src/app/editdelete.tsx
+++ src/app/editdelete.tsx
@@ -3,7 +3,7 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import { deleteUser, updateUser } from "./lib/action";
 import { Display } from "./adduser";
-import { Alert, Backdrop, CircularProgress, Snackbar, SnackbarCloseReason, Tooltip } from "@mui/material";
+import { Alert, Backdrop, Box, Button, CircularProgress, Modal, Snackbar, SnackbarCloseReason, Tooltip, Typography } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 
 interface UserProps {
@@ -19,6 +19,7 @@ export default function EditAndDelete({ id, setUserData }: UserProps) {
     const [failedAlert, setfailedAlert] = useState(false);
     const [message, setMessage] = useState('')
     const [loading, setLoading] = useState(false)
+    const [open, setOpen] = useState(false);
 
     const handleClose = (
         event?: React.SyntheticEvent | Event,
@@ -55,6 +56,7 @@ export default function EditAndDelete({ id, setUserData }: UserProps) {
     };
 
     const handleDelete = async (id: number) => {
+        setOpen(false)
         setLoading(true)
         const deleted = await deleteUser(id)
         if (deleted.success === true) {
@@ -84,7 +86,34 @@ export default function EditAndDelete({ id, setUserData }: UserProps) {
             </form>}
             <div className="flex flex-wrap justify-center gap-x-8 gap-y-4 px-4 md:px-0 py-2">
                 <button onClick={() => setisEdit(id)} className="bg-green-400 text-white px-8 py-2 w-full md:w-auto">Edit</button>
-                <button onClick={() => { handleDelete(id) }} className="bg-gray-500 text-white px-8 py-2 w-full md:w-auto">Delete</button>
+                <button onClick={() => setOpen(true)} className="bg-gray-500 text-white px-8 py-2 w-full md:w-auto">Delete</button>
+                <Modal
+                    open={open}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={{
+                        position: 'absolute' as 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        boxShadow: 24,
+                        p: 4,
+                    }}>
+                        <Typography className="text-red-500 font-semibold" id="modal-modal-title" variant="h6" component="h2">
+                            Are You Sure
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+                            You Want To Delete This User???
+                        </Typography>
+                        <div className="py-2">
+                            <Button onClick={() => setOpen(false)} className="text-xl" sx={{ textTransform: 'none' }}>No</Button>
+                            <Button onClick={() => handleDelete(id)} className="text-xl" sx={{ textTransform: 'none' }}>Yes</Button>
+                        </div>
+                    </Box>
+                </Modal>
             </div>
             <Snackbar open={successAlert} autoHideDuration={4000} onClose={handleClose}>
                 <Alert
